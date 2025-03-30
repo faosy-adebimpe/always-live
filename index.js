a2cc2cfbaceb2e478c8c7ef3629a46cb2add5189
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-03-29 17:30:47 -0700

    add interval to fetch the server at specific timeframe

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+const axios = require('axios');
+
 require('dotenv').config();
 
 // port and other vars
@@ -10,8 +12,15 @@ const port = process.env.PORT || 5000;
 // routers
 app.get('/', (req, res) => {
 	res.send('always-live...');
+	console.log('site accessed');
 });
 
 
 // start server
 app.listen(port, () => console.log(`server running on: http://127.0.0.1:${port}`));
+
+
+setInterval( async () => {
+	const response = await axios.get('https://always-live.onrender.com/');
+	console.log(response.data);
+}, 2000)
